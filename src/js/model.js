2d09179645f97a0d1b99d21529b3b2cde79db54f
imrantahir16 <digitronicdev@gmail.com>
2022-03-02 18:46:24 +0500

    change fetching url

--- src/js/model.js
+++ src/js/model.js
@@ -32,7 +32,7 @@ const createRecipeObject = function (data) {
 export const loadRecipe = async function (id) {
   try {
     // const data = await AJAX(`${API_URL}${id}?key=${KEY}`);
-    const data = await AJAX(`${API_URL}${id}`);
+    const data = await AJAX(`${API_URL}${id}?key=${KEY}`);
     state.recipe = createRecipeObject(data);
     if (state.bookmarks.some(bookmark => bookmark.id === id))
       state.recipe.bookmarked = true;
